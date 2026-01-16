5fe2826fee0a63636aa1a18e31ce072961f76641
Dhruv <dhruvchaudhary1611@gmail.com>
2026-01-16 21:18:08 +0530

    views are changed

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 from . import views
 
+router = SimpleRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
 
-urlpatterns = [
-    path('products/', views.ProductList.as_view(), name='product_list'),
-    path('products/<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
-    path('collections/', views.CollectionList.as_view(), name='collection_list'),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection_detail'),
-]
+
+urlpatterns = router.urls
+    
+
+
+
--- store/views.py
+++ store/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Product, Collection
+from .models import Product, Collection, OrderItem
 from .serializers import CollectionSerializer, ProductSerializer
 from django.db.models import Count
 from rest_framework.views import APIView
@@ -20,25 +20,23 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'request': self.request}
     
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
+        return super().destroy(request, *args, **kwargs)
+    
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(
         product_count=Count('products')
     )
     serializer_class = CollectionSerializer
 
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        if collection.products.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
             return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
+
+
 
     
 
