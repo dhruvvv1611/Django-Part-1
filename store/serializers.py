2c7f3907a7d82c7143c57b9ea36cec7cbcb55d20
Dhruv <dhruvchaudhary1611@gmail.com>
2026-01-17 17:14:50 +0530

    Added Cart

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 from rest_framework import serializers
-from store.models import Product, Collection, Review
+from store.models import Product, Collection, Review, Cart, CartItem
 
 class CollectionSerializer(serializers.ModelSerializer):
 
@@ -30,10 +30,38 @@ class ReviewSerializer(serializers.ModelSerializer):
         product_id = self.context['product_id']
         return Review.objects.create(product_id=product_id, **validated_data)
         
-    
+class CartItemProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price']   
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = CartItemProductSerializer()
+    total_price = serializers.SerializerMethodField(method_name='get_total_price')
+    class Meta:
+        model= CartItem
+        fields = ['id', 'product', 'quantity', 'total_price']
+
+    def get_total_price(self, cart_item: CartItem):
+        return cart_item.quantity * cart_item.product.unit_price
 
     
-   
+
+class CartSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+    items = CartItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField(method_name='get_total_price')
+    class Meta:
+        model = Cart
+        fields = ['id','items','total_price']
+
+    def get_total_price(self, cart: Cart):
+        return sum([item.quantity * item.product.unit_price for item in cart.items.all()])
+        
+
+    
+
+
 
 
     
--- store/views.py
+++ store/views.py
@@ -3,12 +3,13 @@ from rest_framework.response import Response
 from rest_framework import status
 from store.filters import ProductFilter
 from store.pagination import DefaultPagination
-from .models import Product, Collection, OrderItem, Review
-from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
+from .models import Product, Collection, OrderItem, Review, Cart, CartItem
+from .serializers import CartItemSerializer, CollectionSerializer, ProductSerializer, ReviewSerializer, CartSerializer
 from django.db.models import Count
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 
 
 class ProductViewSet(ModelViewSet):
@@ -50,6 +51,18 @@ class ReviewViewSet(ModelViewSet):
     
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
+    
+
+class CartViewSet(CreateModelMixin,RetrieveModelMixin,GenericViewSet,DestroyModelMixin):
+    queryset = Cart.objects.prefetch_related('items__product').all()
+    serializer_class = CartSerializer
+
+class CartItemViewSet(ModelViewSet):
+    serializer_class = CartItemSerializer
+    
+    def get_queryset(self):
+        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk']).select_related('product')
+    
     
 
 
